762d368fa281ab80e0a939eb338de365069c4508
coffeemug17 <mitulpandey2@gmail.com>
2023-03-21 15:47:44 -0700

    Changing routes and components according to project needs

--- src/pages/MyNotesPage/MyNotesPage.jsx
+++ src/pages/MyNotesPage/MyNotesPage.jsx
@@ -1,6 +1,6 @@
 import * as usersService from "../../utilities/users-service";
 
-export default function OrderHistoryPage() {
+export default function MyNotesPage() {
   async function handleCheckToken(evt) {
     const expDate = await usersService.checkToken();
     console.log(expDate);
@@ -8,7 +8,7 @@ export default function OrderHistoryPage() {
 
   return (
     <>
-      <h1>OrderHistoryPage</h1>
+      <h1>My Previously Created Notes</h1>
       <button onClick={handleCheckToken}>Check When My Login Expires</button>
     </>
   );
---
+++ src/pages/NewNotePage/NewNotePage.jsx
@@ -0,0 +1,5 @@
+export default function NewNotePage() {
+  return (
+    <h1>Create a New Note</h1>
+  );
+}
